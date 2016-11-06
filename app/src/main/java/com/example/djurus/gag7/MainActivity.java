ef444b8b89f7830e793d8bd0147b83f876ca6dd3
Shaun Djuhari <shaundjuhari@berkeley.edu>
2016-11-05 18:49:10 -0700

    tab functionality

---
+++ app/src/main/java/com/example/djurus/gag7/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.djurus.gag7;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.djurus.gag7.R;
+import com.example.djurus.gag7.FreshFragment;
+import com.example.djurus.gag7.HotFragment;
+import com.example.djurus.gag7.TrendingFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements HotFragment.OnFragmentInteractionListener,
+        TrendingFragment.OnFragmentInteractionListener,
+        FreshFragment.OnFragmentInteractionListener{
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HotFragment(), "HOT");
+        adapter.addFragment(new TrendingFragment(), "TRENDING");
+        adapter.addFragment(new FreshFragment(), "FRESH");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+    public void onFragmentInteraction(Uri uri){
+    }
+}
